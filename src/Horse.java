147fb3306bd892e4fe22e019ff9484e74b73c44e
philipp-schuetz <philipp.schuetz@protonmail.com>
2023-01-22 13:49:14 +0100

    Add horse functionality

--- src/Horse.java
+++ src/Horse.java
@@ -13,17 +13,24 @@ public class Horse extends Animal
         }
     }
 
+    // code executed when feeding horse
     private void feedHorse(){
         Data data = new Data();
         if (data.items[1] > 0){
-            // remove one carrot from inventory
+            // remove one carrot from inventory for feeding
             data.items[1] -= 1;
             data.write();
 
             Carrot carrot = new Carrot();
             getWorld().addObject(carrot, 25, 2);
+            
             Greenfoot.delay(80);
             getWorld().removeObject(carrot);
+            
+            // turn horse on feeding
+            this.turn(20);
+            Greenfoot.delay(20);
+            this.turn(-20);
         }
     }
 }
